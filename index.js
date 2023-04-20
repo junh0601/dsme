0777274072d90f2adb47ff8183778e695671bc83
Junhyeok Lee <junh0601@naver.com>
2023-04-20 20:31:26 +0900

    ..

--- index.js
+++ index.js
@@ -103,46 +103,9 @@ if ((hour >= 19 && min >= 45) || hour >= 20) {
   getBusFilter(weekday, "퇴근", "18", dest).then((result) => {
     paintBusData(result, weekday, "퇴근", "18", dest);
   });
-} else if (hour >= 12) {
+} else {
   getBusFilter(weekday, "퇴근", "17", dest).then((result) => {
     paintBusData(result, weekday, "퇴근", "17", dest);
-  });
-}
-
-// 정오 연장 알림
-if (hour === 12) {
-  const article = document.createElement("article");
-  const cards = document.getElementById("cards");
-  article.style = "order:-2;";
-  article.innerHTML = ` 점심 시간 연장 여부 조회중`;
-  article.ariaBusy = "true";
-  cards.appendChild(article);
-  getWeatherData({ mode: "check", value: "12:00" }).then((data) => {
-    if (data !== null) {
-      let msg = "";
-      if (parseFloat(data[8]) >= 31.5) {
-        msg = "1시간 연장입니다.";
-      } else if (parseFloat(data[8]) >= 28) {
-        msg = "30분 연장입니다.";
-      } else {
-        msg = "연장이 아닙니다.";
-      }
-      article.innerHTML = `
-        <header>🚨 점심 시간 <mark>${msg}</mark></header>
-        <center><hgroup><h2> ${data[8]}C°</h2><h6>12:00시 기준</h6> <hgroup></center>
-        <footer>
-          <small>
-            <div><i class="fa-solid fa-info"></i> 연장 기준은 28도 이상은 30분 연장, 31.5도 이상은 1시간 연장 (12시 정각 온도)</div>
-            <div>
-              <i class="fa-solid fa-info"></i> <a href="http://www.kma.go.kr/cgi-bin/aws/nph-aws_txt_min?0&0&MINDB_01M&294&a" class="contrast" target="_blank">공공데이터</a>를 기반으로 한 결과이며, 정확한 결과는
-              <a href="https://partners.dsme.co.kr/" class="contrast"  target="_blank">사내 공지</a>를 확인해주세요.
-            </div>
-          </small>
-        </footer>`;
-    } else {
-      article.innerHTML = "점심 연장 여부가 아직 조회되지 않습니다. (12시 정각의 기온은 대략 12:04분에 발표됩니다)";
-    }
-    article.ariaBusy = "false";
   });
 }
 
